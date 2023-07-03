0917ac31f6b24edf6c3d3331448131c5480a39c9
22Sunatullo <muslimamuminova6969@gmail.com>
2023-07-03 16:06:41 +0500

    git p

--- src/components/Infrastructure/Infrastructure.tsx
+++ src/components/Infrastructure/Infrastructure.tsx
@@ -1,9 +1,29 @@
 import styles from './Infrastructure.module.scss'
 
 export const Infrastructure = () => {
-  return (
-    <div className={styles.container}>
-       
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <div className={styles.item1}>
+                <img src="/assets/infrastructure/1.png" alt="" />
+            </div>
+            <div className={styles.item2}>
+                <img src="/assets/infrastructure/2.png" alt="" />
+            </div>
+            <div className={styles.item3}>
+                <img src="/assets/infrastructure/3.png" alt="" />
+            </div>
+            <div className={styles.item4}>
+                <img src="/assets/infrastructure/4.png" alt="" />
+            </div>
+            <div className={styles.item5}>
+                <img src="/assets/infrastructure/5.png" alt="" />
+            </div>
+            <div className={styles.item6}>
+                <img src="/assets/infrastructure/6.png" alt="" />
+            </div>
+            <div className={styles.item7}>
+                <img src="/assets/infrastructure/7.png" alt="" />
+            </div>
+        </div>
+    )
 }
